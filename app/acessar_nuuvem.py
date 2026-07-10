9a2abfaa3ccc8d13cfbe5732bbce9a0090cbcf26
DouglasCruzz10 <douglascruztst11@gmail.com>
2026-07-10 17:45:48 -0400

    fix: Função de click no menu reinserida.

--- app/acessar_nuuvem.py
+++ app/acessar_nuuvem.py
@@ -1,4 +1,6 @@
 def abrir_nuuvem(pagina):
-    pagina.goto("https://www.nuuvem.com/")
+    pagina.goto("https://www.nuuvem.com/br-pt")
+    pagina.get_by_title("Menu").click()
+    pagina.get_by_role("link", name=" PC ").click()
     pagina.get_by_role("link", name="Ofertas", exact=True).click()
     pagina.wait_for_selector("a[data-default-tracker-product-id-param]")
